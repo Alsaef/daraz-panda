f53114d9652466add44a6ebd759a17d7b9fdcc3b
ASR <saef.ratul@gmail.com>
2023-05-29 13:49:46 +0600

    dee

--- script.js
+++ script.js
@@ -30,3 +30,19 @@ function nightMood() {
 document.getElementById('mood-remove').addEventListener("click",function (event) {
     event.target.parentNode.removeChild(event.target)
 })
+
+// review
+
+const reviews= document.getElementsByClassName("fa-star");
+for (const review of reviews) {
+    review.addEventListener("click",function () {
+        review.style.color="#ffd700"
+    })
+}
+
+function clickReview() {
+    const textReview=document.getElementById("text-review");
+    textReview.innerText="Review succes";
+    textReview.style.backgroundColor=" bisque"
+    textReview.style.padding="10px"
+}
